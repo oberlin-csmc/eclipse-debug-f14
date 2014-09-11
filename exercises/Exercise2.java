ffaa860c1e67c4309ac63c9723650372cfe0d68f
Daniel Barella <dan.barella@gmail.com>
2014-09-11 19:07:37 -0400

    Modified the exercises.

--- exercises/Exercise2.java
+++ exercises/Exercise2.java
@@ -5,8 +5,9 @@ package exercises;
  * 
  * @author dbarella@oberlin.edu (Dan Barella)
  * 
- * There are two bugs in each of these exercises, one which crashes the
- * code and another where it will run but give an incorrect output.
+ * There are two main kinds of bugs which we need to worry about: 
+ * The kind that crashes the code, and the kind which will run but give an
+ * incorrect output. At Google we called the second kind "Correctness bugs".
  * 
  * Bugs which crash your code are generally easier to find, so most
  * people concentrate on those first. However, correctness bugs are just
@@ -14,18 +15,21 @@ package exercises;
  * correctness bug, the debugger is your best friend -- but the only
  * tried-and-true way to avoid introducing them in the first place is to
  * write good unittests.
+ * 
+ * In these exercises there is a mix of crashing bugs and correctness bugs.
  */
 public class Exercise2 {
 
     /**
      * Exercise 2.1 - Prints the multiplication table from 1 to max.
+     * Just correctness bugs here.
      * @param max The max number to print.
      */
     public static void printMultiplicationTable1(int max) {
 	int floor = 1;
-	for(int i=floor; i<max; i=i++) {
+	for(int i=floor; i<max; i++) {
 	    for(int j=floor; j<max; j++) {
-		System.out.printf("%3d ", i+j);
+		System.out.printf("%3d ", i*j);
 	    }
 	    System.out.println();
 	}
@@ -34,8 +38,8 @@ public class Exercise2 {
     /**
      * Exercise 2.2 - Prints the multiplication table from 1 to max.
      * 
-     * This function has the same bugs as @printMultiplicationTable1, but
-     * with one more correctness bug -- just a programming subtlety.
+     * This function is similar to @printMultiplicationTable1, but
+     * with some subtler bugs -- one crashing and one correctness.
      * Feel free to skip this and come back to it another time.
      * 
      * @param max The max number to print.
@@ -44,7 +48,7 @@ public class Exercise2 {
 	int floor = 1;
 	for(int i=floor; i<max; i=i++) {
 	    for(int j=floor; j<max; j++) {
-		System.out.printf("%3o ", i+j);
+		System.out.printf("%3o ", i*j);
 	    }
 	    System.out.println();
 	}
@@ -55,7 +59,7 @@ public class Exercise2 {
 	printMultiplicationTable1(12);
 	
 	// Uncomment these lines for an extra challenge
-	System.out.println("========== Exercise 2.2 ==========");
+	//System.out.println("========== Exercise 2.2 ==========");
 	//printMultiplicationTable2(12);
     }
 
