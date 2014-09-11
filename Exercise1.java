af0bd331eb4a2efd8a4b99d65bb5fe99b7a455a1
Daniel Barella <dan.barella@gmail.com>
2014-09-11 17:12:04 -0400

    Initial exercise 1.

---
+++ Exercise1.java
@@ -0,0 +1,14 @@
+/**
+ * Debugging exercise 1
+ * @author dbarella@oberlin.edu (Dan Barella)
+ *
+ */
+public class Exercise1 {
+
+    public static void main(String[] args) {
+	for(int i=0; i<=args.length; i++) {
+	    System.out.println(args[i]);
+	}
+    }
+
+}
